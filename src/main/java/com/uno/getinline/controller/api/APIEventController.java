c92b285c317e4e64fe5dc73485afd11b6ddd788d
PCY <pcy4196@naver.com>
2022-06-01 23:35:03 +0900

    implement #2 - APIAuthControllerTest외 1건 구현 2022.06.01 - 001 MockMvc 를 사용하여 API controllerTest 구현, dto 추가, controller 수정

--- src/main/java/com/uno/getinline/controller/api/APIEventController.java
+++ src/main/java/com/uno/getinline/controller/api/APIEventController.java
@@ -1,13 +1,13 @@
 package com.uno.getinline.controller.api;
 
-import com.uno.getinline.constant.ErrorCode;
-import com.uno.getinline.dto.APIErrorResponse;
-import com.uno.getinline.exception.GeneralException;
+import com.uno.getinline.constant.EventStatus;
+import com.uno.getinline.dto.APIDataResponse;
+import com.uno.getinline.dto.EventRequest;
+import com.uno.getinline.dto.EventResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequestMapping("/api")
@@ -15,46 +15,54 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() throws Exception {
-//        throw new GeneralException("테스트 메시지");
-        throw new HttpRequestMethodNotSupportedException("GET");
-//        return List.of("event1", "event2");
+    public APIDataResponse<List<EventResponse>> getEvents() {
+        return APIDataResponse.of(List.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        )));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/events")
-    public Boolean createEvent() {
-        throw new RuntimeException("runtime 테스트 메시지");
-//        return true;
+    public APIDataResponse<Void> createEvent(@RequestBody EventRequest eventRequest) {
+        return APIDataResponse.empty();
     }
 
     @GetMapping("/events/{eventId}")
-    public String getEvent(@PathVariable Integer eventId) {
-        throw new GeneralException("테스트 메시지");
-//        return "event" + eventId;
+    public APIDataResponse<EventResponse> getEvent(@PathVariable Long eventId) {
+        if (eventId.equals(2L)) {
+            return APIDataResponse.empty();
+        }
+
+        return APIDataResponse.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        ));
     }
 
     @PutMapping("/events/{eventId}")
-    public Boolean modifyEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> modifyEvent(
+            @PathVariable Long eventId,
+            @RequestBody EventRequest eventRequest
+    ) {
+        return APIDataResponse.empty();
     }
 
     @DeleteMapping("/events/{eventId}")
-    public Boolean deleteEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> removeEvent(@PathVariable Long eventId) {
+        return APIDataResponse.empty();
     }
 
-//    @ExceptionHandler
-//    public ResponseEntity<APIErrorResponse> general(GeneralException e) {
-//        ErrorCode errorCode = e.getErrorCode();
-//        HttpStatus status = errorCode.isClientSideError() ?
-//                HttpStatus.BAD_REQUEST :
-//                HttpStatus.INTERNAL_SERVER_ERROR;
-//        return ResponseEntity
-//                .status(status)
-//                .body(APIErrorResponse.of(
-//                        false,
-//                        errorCode,
-//                        errorCode.getMessage(e)
-//                ));
-//    }
 }
